c5374226f8ac97fc5df10bbef999c2794c729c5e
Nabil <messaoudnabil9@gmail.com>
2021-04-07 22:17:20 +0100

    deployement7

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "semantic-ui-css/semantic.min.css";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { configureStore } from "./store/configureStore";
 import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
 import ReduxToastr from "react-redux-toastr";
 import "react-datepicker/dist/react-datepicker.css";
+import "./semantic-ui-css/semantic.min.css";
 
 const store = configureStore();
 
