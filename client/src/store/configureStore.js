dec09da710f55079df1eb34830859b64be0057ec
Nabil <messaoudnabil9@gmail.com>
2021-06-29 18:50:50 +0100

    compress files2

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
 
 export const configureStore = () => {
   const middlewares = [thunk];
-  const conposedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
+  const conposedEnhancer = applyMiddleware(...middlewares);
   const store = createStore(rootReducer, conposedEnhancer);
 
   return store;
