83ebe3e4cc3afbdf8830aefe75336f550575c9e7
Nabil <messaoudnabil9@gmail.com>
2021-07-02 12:06:10 +0100

    compress res2

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ const path = require("path");
 const socketio = require("socket.io");
 const newMsg = require("./routes/msgRoute");
 const chechoutCompleted = require("./middlewares/chechoutCompleted");
+var compression = require("compression");
 
 //configuration
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(compression());
 
 //deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res, next) => {
     let url = req.originalUrl;
+    res.setHeader("Accept-Encoding", "gzip, compress, br");
     if (url.startsWith("/uploads")) {
       let file = url.slice(16);
       res.sendFile(path.resolve(__dirname, "uploads", "images", file));
