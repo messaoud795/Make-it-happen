055a962c5de3fbda1f26ed49c54b0a099b6f5306
nabil <messaoudnabil9@gmail.com>
2024-05-26 12:05:50 +0100

    add build

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,13 +1,14 @@
-import { applyMiddleware } from "redux";
-import { legacy_createStore as createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../reducers/rootReducer";
-import thunk from "redux-thunk";
+import { applyMiddleware } from 'redux';
+// import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducer';
+import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
 export const configureStore = () => {
   const middlewares = [thunk];
   let conposedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'production') {
     conposedEnhancer = applyMiddleware(...middlewares);
   }
   const store = createStore(rootReducer, conposedEnhancer);
